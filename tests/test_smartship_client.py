1a1e09645fbf530cc90e49a0ef416500a385fe1e
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2017-04-05 14:29:17 +0300

    Add client support for getting PDF data from shipment responses

--- tests/test_smartship_client.py
+++ tests/test_smartship_client.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from mock import Mock, patch
+from pytest import raises
 from requests.auth import HTTPBasicAuth
 
 from smartship import Client
@@ -37,3 +38,29 @@ def test_send_shipment(smartship_client, simple_shipment):
     simple_shipment.build.assert_called_once_with()
     smartship_client._post.assert_called_once_with("/shipments", simple_shipment.data)
     assert response == "response"
+
+
+@patch("smartship._client.requests.get", return_value=Mock(status_code=200, content="pdfdata"))
+def test_get_pdf_data(mock_get, smartship_client):
+    href = "%s/shipments/%d/pdfs/%d" % (Client.SMARTSHIP_API_ENDPOINT, 1, 2)
+    content = [{
+        "id": 1,
+        "pdfs": [{
+            "id": 2,
+            "href": href,
+        }],
+    }]
+    response = Mock(status_code=201)
+    response.json = Mock(return_value=content)
+    pdf_data = smartship_client.get_pdf_data(response)
+    assert pdf_data == [["pdfdata"]]
+    mock_get.assert_called_once_with(
+        href,
+        params=None,
+        auth=HTTPBasicAuth("username", "secret")
+    )
+
+
+def test_get_pdf_data_fail(smartship_client):
+    with raises(ValueError):
+        smartship_client.get_pdf_data(Mock(status_code=200))
